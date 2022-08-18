338d86fea1e343ef248022dfdf59200dd8f7f35a
Sash <notsadako@yandex.ru>
2022-08-18 18:00:20 +0300

    new commit

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -22,6 +22,7 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+
     }
 
     @Override
