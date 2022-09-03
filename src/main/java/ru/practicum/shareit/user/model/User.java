64f0175ae47532546f521e89ed69f6dc69ec8e20
Sash <notsadako@yandex.ru>
2022-09-04 02:42:39 +0300

    Add booking

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -26,13 +26,6 @@ public class User {
     @Column(name = "user_email")
     private String email;
 
-    //это видимо удалить, не помню зачем пихнула
-    /*@OneToMany(mappedBy = "owner")
-    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    private Set<Item> items;*/
-
     public User() {
     }
 }
