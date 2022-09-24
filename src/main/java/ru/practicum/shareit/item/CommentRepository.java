f1579828ebbb8ef82a27aeb0f9fb3504ac689c86
Sash <notsadako@yandex.ru>
2022-09-24 17:02:42 +0300

    Fix checkstyle violations

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -4,11 +4,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.shareit.item.dto.ItemDtoWithDate;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
